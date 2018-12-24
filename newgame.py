a8dbd8ea38b117b8291ce636836901262b943c52
michaelandsylas234 <46094186+michaelandsylas234@users.noreply.github.com>
2018-12-24 18:58:22 -0500

    Update newgame.py

--- newgame.py
+++ newgame.py
@@ -7,41 +7,55 @@ def roll(sided, times):
     return sum
 
 
+class Monster:
+    def __init__(self, name, action, hits):
+        self.name = name
+        self.action = action
+        self.hits = hits
+
 
 print("\n-------------------------------------------------------------------------------------------------\n")
 
-doyouwant = input("You see a BSOD.  It wants to kill your computer!!! Fight (f) or run (r)? ")
+monster1 = Monster("giant rat", "kill you!", 5)
+monster2 = Monster("sheep", "baaaaa you!", 1)
+
+if roll(2, 1) > 1:
+    monster = monster1
+else:
+    monster = monster2
+
+doyouwant = input(f"You see a {monster.name}!!!  It wants to {monster.action}!!! Fight (f) or run (r)? ")
 if doyouwant[0] != "f":
-    print("ok, bye")
+    print("Last person to play this game's a rot13 egg!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 else:
     mypoints = 10
-    bsodpoints = 5
     turn_number = 1
 
-    while mypoints > 0 and bsodpoints > 0:
+    while mypoints > 0 and monster.hits > 0:
         print("\n-------------------------------------------------------------------------------------------------\n")
         print(f"TURN #{turn_number}")
 
         input("your move.  press f to continue fighting. ")
         yourmove = roll(20, 1)
         if yourmove > 7:
-            bsodpoints = bsodpoints - 1
-            print(f"you hit the BSOD.  it has {bsodpoints} left.")
+            monster.hits = monster.hits - 1
+            print(f"you hit the {monster.name}.  it has {monster.hits} left.")
         else:
-            print("you missed the BSOD.  sorry!")
+            print(f"you missed the {monster.name}.  sorry!")
 
         bsodmove = roll(20, 1)
         if bsodmove > 11:
             mypoints = mypoints - 2
-            print(f"the BSOD hit you.  you have {mypoints} left.")
+            print(f"the {monster.name} hit you.  you have {mypoints} left.")
         else:
-            print("the bsod missed you.  lucky!")
+            print(f"the {monster.name} missed you.  lucky!")
         
         turn_number = turn_number + 1
 
-    if bsodpoints <= 0 and mypoints < 0:
-        print("YOU AND THE BSOD BOTH DIED")
-    elif bsodpoints <= 0:  # elif is short for "else if"
-        print("BSOD is DEAD")
+    if monster.hits <= 0 and mypoints < 0:
+        print(f"YOU AND THE {monster.name} BOTH DIED")
+    elif monster.hits <= 0:  # elif is short for "else if"
+        print(f"The {monster.name} is DEAD")
     else:
         print("YOU ARE DEAD")
+input("Press Enter to close this window...\n")
